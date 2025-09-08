b4d07cddfbc3b479ee7ac78c7464484ad3dad0a9
PrakharTagra <prakhartagra16@gmail.com>
2025-09-08 17:24:44 +0530

    Preet

---
+++ psy.js
@@ -0,0 +1,11 @@
+for(let i=1;i<=30;i++){
+    let qbox = document.querySelector(`.qbox${i}`);
+    let sub = document.querySelector(`.sbt${i}`);
+    
+    sub.addEventListener("click",()=>{
+        qbox.style.display="none";
+    })
+}
+for(i=1;i<=30;i++){
+    qbox
+}
