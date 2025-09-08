9d4554defeaa3f1ca777b610a9157afb61ba45c7
PrakharTagra <prakhartagra16@gmail.com>
2025-09-08 21:58:27 +0530

    psychometric test

--- psy.js
+++ psy.js
@@ -1,11 +1,46 @@
-for(let i=1;i<=30;i++){
-    let qbox = document.querySelector(`.qbox${i}`);
-    let sub = document.querySelector(`.sbt${i}`);
-    
-    sub.addEventListener("click",()=>{
-        qbox.style.display="none";
+const question = document.querySelectorAll(".question");
+const submit = document.querySelector(".sbt");
+const bar = document.querySelector(".ipbar");
+const qh = document.querySelector(".qh");
+const section = document.querySelector(".section");
+let currentIndex = 0;
+
+function showQuestion(index){
+    question.forEach((q,i)=>{
+        q.classList.toggle("active",i===index);
     })
 }
-for(i=1;i<=30;i++){
-    qbox
-}
+
+submit.addEventListener("click",()=>{
+    const currentQuestion = question[currentIndex];
+    const selected = currentQuestion.querySelector("input[type='radio']:checked");
+
+    if(!selected){
+        alert("Please select any option before continuing!!");
+        return;
+    }
+
+    currentIndex++;
+    const progress = (currentIndex/question.length)*100;
+    bar.style.width= progress + "%";
+    if(currentIndex<question.length){
+        showQuestion(currentIndex);
+    }
+    else{
+        submit.style.display="none"
+    }
+    for(let i=0;i<10;i++){
+        if(currentIndex<=10){
+            qh.innerHTML=`Question ${currentIndex+1} of 10`;
+        }
+        if(currentIndex>=10 && currentIndex<=20){
+            qh.innerHTML=`Question ${currentIndex-9} of 10`;  
+            section.innerHTML="Behaviour" 
+        }
+        if(currentIndex>=20 && currentIndex<=30){
+            qh.innerHTML=`Question ${currentIndex-19} of 10`;
+            section.innerHTML="Mental & Psychology" 
+        }
+    }
+})
+showQuestion(currentIndex);
