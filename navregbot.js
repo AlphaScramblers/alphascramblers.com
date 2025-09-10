156a9d5d1474ff57cc89f8a10e248e56ce2b9019
aryan rai <aryanrai1402@gmail.com>
2025-09-10 16:49:19 +0530

    update

--- navregbot.js
+++ navregbot.js
@@ -331,7 +331,7 @@ logsub.addEventListener("click", async (e) => {
     });
     const data = await res.json();
     if (data.success) {
-        localStorage.setItem("userProfile", "true");
+        localStorage.setItem("loggedIn", "true");
         error2.style.display = "none";
         login.style.display="none";
         login1.style.display="none";
