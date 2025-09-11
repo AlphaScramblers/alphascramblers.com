3885f4acd3f8b0aa8746f4ec3586d82c0c61da3a
PrakharTagra <prakhartagra16@gmail.com>
2025-09-11 19:16:34 +0530

    js

--- AboutUs/aboutus.js
+++ AboutUs/aboutus.js
@@ -1,19 +1,3 @@
-for(let i=1;i<=12;i++){
-    let button = document.querySelector(`.C${i}`);
-    let fc = document.querySelector(`.fc${i}`);
-    let fcnew = document.querySelector(`.fc${i}n`)
-    button.addEventListener("click",()=>{
-    if(fc.classList.contains(`fc${i}`)){
-        fc.classList.remove(`fc${i}`);
-        fc.classList.add(`fc${i}n`);
-        fc.scrollIntoView();
-    }
-    else{
-        fc.classList.remove(`fc${i}n`);
-        fc.classList.add(`fc${i}`)
-    }
-})
-}
 document.addEventListener("DOMContentLoaded", () => {
   const loggedIn = localStorage.getItem("loggedIn");
   if (loggedIn) {
