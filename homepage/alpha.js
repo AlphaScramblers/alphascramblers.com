1a29e8f5bb727163e0913dd2e057048f93218a4c
PrakharTagra <prakhartagra16@gmail.com>
2025-09-11 19:04:24 +0530

    hi

--- homepage/alpha.js
+++ homepage/alpha.js
@@ -76,3 +76,39 @@ d4.addEventListener("click",()=>{
     d3.style.backgroundColor="white";
     d4.style.backgroundColor="gray";
 })
+document.addEventListener("DOMContentLoaded", () => {
+  const loggedIn = localStorage.getItem("loggedIn");
+  if (loggedIn) {
+    login.style.display = "none";
+    login1.style.display = "none";
+
+    if (tab.matches) {
+      profile.style.display = "block";
+    }
+    if (mobile.matches) {
+      profile1.style.display = "block";
+    }
+    if (lap.matches) {
+      profile.style.display = "block";
+    }
+    logsignin.innerHTML = "Signed In Successfully";
+    logsignin.disabled = true;
+    logname.readOnly = true;
+    logpass2.readOnly = true;
+  } else {
+    login.style.display = "block";
+    login1.style.display = "block";
+    profile.style.display = "none";
+    profile1.style.display = "none";
+  }
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const link = document.querySelector(".link1"); 
+  link.addEventListener("click", (e) => {
+    const loggedIn = localStorage.getItem("loggedIn");
+    if (!loggedIn) {
+      e.preventDefault(); 
+      alert("Please log in first to access the Psychometric Test page!");   
+    }
+  });
+});
