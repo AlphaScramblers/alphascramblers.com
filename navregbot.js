e7c5e5f9fd60cc612c85788d4b6a5cff93572166
aryan rai <aryanrai1402@gmail.com>
2025-09-13 02:50:06 +0530

    Revert "profile"
    This reverts commit 168a2f9243e06604834aaaafe2df079b9fb8381d.

--- navregbot.js
+++ navregbot.js
@@ -281,9 +281,6 @@ logsub.addEventListener("click", async (e) => {
         });
         const data = await res.json();
         if (data.success) {
-             localStorage.setItem('userFirstName', firstName);
-    localStorage.setItem('userLastName', lastName);
-
             name1.innerHTML = firstName + lastName
             logfname.readOnly = true
             loglname.readOnly = true
