2e8246ab059271c28634ee7b7322845e4753952b
PrakharTagra <prakhartagra16@gmail.com>
2025-09-13 12:13:29 +0530

    Update navregbot.js

--- navregbot.js
+++ navregbot.js
@@ -392,8 +392,4 @@ profile1.addEventListener("click", () => {
         profilesection.classList.add("show");
         state1 = "vis";
     }
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> parent of e0a085e (logout)
