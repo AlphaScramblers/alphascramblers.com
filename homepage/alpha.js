bd5450c7063dab98ca2aa4393d15981bc31bf134
aryan rai <aryanrai1402@gmail.com>
2025-09-14 13:49:54 +0530

    dynamic ui

--- homepage/alpha.js
+++ homepage/alpha.js
@@ -81,6 +81,7 @@ document.addEventListener("DOMContentLoaded", () => {
 let mobile = window.matchMedia("(max-width: 700px)");
 let lap = window.matchMedia("(min-width: 700px)");
 const mainprof = document.querySelector(".profile-section")
+const naam = document.querySelector(".namedata");
 
    const login      = document.querySelector(".login");
   const login1     = document.querySelector(".login1");
@@ -130,6 +131,7 @@ let logpass2 = document.getElementById("logpass2");
     login.style.display = "block";
     login1.style.display = "block";
     mainprof.style.display="none";
+    naam.innerHTML=phla+" " +dusra 
 
     if (tab.matches) {
       profile.style.display = "none";
