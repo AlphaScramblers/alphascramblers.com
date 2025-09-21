fe272ed6bbb278fbd03e421dd529834d0dc83a3a
PrakharTagra <prakhartagra16@gmail.com>
2025-09-21 18:01:11 +0530

    Update helpandsupport.js

--- helpandsupport/helpandsupport.js
+++ helpandsupport/helpandsupport.js
@@ -18,3 +18,29 @@ for(let i=1;i<=10;i++){
     }
 })
 }
+document.addEventListener("DOMContentLoaded", () => {
+  const loggedIn = localStorage.getItem("loggedIn");
+  if (loggedIn) {
+    login.style.display = "none";
+    login1.style.display = "none";
+
+    if (tab.matches) {
+      profile.style.display = "block";
+    }
+    if (mobile.matches) {
+      profile1.style.display = "block";
+    }
+    if (lap.matches) {
+      profile.style.display = "block";
+    }
+    logsignin.innerHTML = "Signed In Successfully";
+    logsignin.disabled = true;
+    logname.readOnly = true;
+    logpass2.readOnly = true;
+  } else {
+    login.style.display = "block";
+    login1.style.display = "block";
+    profile.style.display = "none";
+    profile1.style.display = "none";
+  }
+});
