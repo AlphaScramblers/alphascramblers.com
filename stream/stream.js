51479714f4cf689e34fdd9f797d82aa533fc09a3
PrakharTagra <prakhartagra16@gmail.com>
2025-09-21 19:57:25 +0530

    Logout

--- stream/stream.js
+++ stream/stream.js
@@ -1,9 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const loggedIn = localStorage.getItem("loggedIn");
-  if (loggedIn) {
+  if (localStorage.getItem("loggedIn") === "true") {
     login.style.display = "none";
     login1.style.display = "none";
-
+    if (tab.matches || lap.matches) {
+      profile.style.display = "block";
+    }
+  } else {
+    if (mobile.matches) {
+      login1.style.display = "block";
+      login.style.display = "none"
+    }
+    if (tab.matches || lap.matches) {
+      login.style.display = "block";
+      login1.style.display = "none";
+    }
+    profile.style.display = "none";
+    profile1.style.display = "none";
+  }
+  const storedFirstName = localStorage.getItem("firstName");
+  const email= localStorage.getItem("email");
+  const no=localStorage.getItem("no");
+  const storedLastName = localStorage.getItem("lastName");
+  const mainprof = document.querySelector(".profile-section")
+  const loggedIn = localStorage.getItem("loggedIn");
+  if (loggedIn) {
+    const avatar = document.querySelector(".avatar");
+    const avatar1 = document.querySelector(".avatar1");
+    const naam = document.querySelector(".namedata");
+    const naam1 = document.querySelector(".namedatahead");
+    const emailva=document.querySelector(".emaildata");
+    const number = document.querySelector(".cnum");
+    login.style.display = "none";
+    login1.style.display = "none";
+    if (naam && storedFirstName && storedLastName &&emailva && number && email && no && avatar) {
+            naam.innerHTML = `${storedFirstName} ${storedLastName}`;
+            naam1.innerHTML = `${storedFirstName} ${storedLastName}`;
+            emailva.innerHTML=`${email}`;
+            number.innerHTML=`${no}`;
+            avatar.innerHTML = `${storedFirstName.charAt(0)}${storedLastName.charAt(0)}`;
+            avatar1.innerHTML = `${storedFirstName.charAt(0)}${storedLastName.charAt(0)}`;
+        }
     if (tab.matches) {
       profile.style.display = "block";
     }
@@ -23,4 +59,34 @@ document.addEventListener("DOMContentLoaded", () => {
     profile.style.display = "none";
     profile1.style.display = "none";
   }
+  const logoutbut=document.querySelector(".lgtBtn")
+  logoutbut.addEventListener("click",() => {
+    localStorage.removeItem("loggedIn")
+    const uploggedIn=localStorage.getItem("loggedIn");
+    if (!uploggedIn) {
+    login.style.display = "block";
+    login1.style.display = "block";
+    mainprof.style.display="none";
+
+    if (tab.matches) {
+      profile.style.display = "none";
+    }
+    if (mobile.matches) {
+      profile1.style.display = "none";
+    }
+    if (lap.matches) {
+      profile.style.display = "none";
+    }
+    logsignin.innerHTML = "Sign In";
+    logsignin.disabled = false;
+    logname.readOnly = false;
+    logpass2.readOnly = false;
+  } else {
+    login.style.display = "none";
+    login1.style.display = "none";
+    profile.style.display = "block";
+    profile1.style.display = "block";
+    } 
+  }
+) 
 });
