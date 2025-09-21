4e2d17546e812d3e119bedcf3b67b41c0d77571c
PrakharTagra <prakhartagra16@gmail.com>
2025-09-21 20:10:51 +0530

    HIii

--- homepage/server.js
+++ homepage/server.js
@@ -1,6 +1,5 @@
 import express from "express";
 import cors from "cors";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 const app = express();
 app.use(cors());
 app.use(express.json());
